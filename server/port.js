4bdf417c5a9b2a6ed2c2f38944bcdb3fa5889a22
Bohdan Koldun <koldunbohdan@gmail.com>
2019-06-25 23:59:28 +0300

    rename app

--- server/port.js
+++ server/port.js
@@ -1,3 +1,3 @@
 const argv = require('./argv');
 
-module.exports = parseInt(argv.port || process.env.PORT || '3000', 10);
+module.exports = parseInt(argv.port || process.env.WEB_PORT || '3000', 10);
