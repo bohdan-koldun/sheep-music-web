599c3bdf4e08229edca9fd5c10daff9e2a367013
Bohdan Koldun <koldunbohdan@gmail.com>
2019-10-29 00:00:36 +0200

    edit video list

--- app/components/ListItem/VideoYoutubeItem.js
+++ app/components/ListItem/VideoYoutubeItem.js
@@ -1,25 +1,51 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player';
 import { Link } from 'react-router-dom';
+import { MdPlayCircleFilled, MdPauseCircleFilled } from 'react-icons/md';
+import getVideoId from 'get-video-id';
 import './VideoYoutubeItem.scss';
 
 function VideoYoutubeItem({ video }) {
+  const [play, setPlay] = useState(false);
   return (
     <div className="song-video-item">
-      <div className="player-wrapper">
-        <ReactPlayer
-          url={video.video}
-          className="react-player"
-          config={{
-            youtube: {
-              playerVars: { showinfo: 0, controls: 1 },
-            },
-          }}
-          width="100%"
-          height="100%"
+      <div
+        className="video-item-picture"
+        onClick={() => {
+          setPlay(!play);
+        }}
+      >
+        <img
+          src={`https://img.youtube.com/vi/${
+            getVideoId(video.video).id
+          }/hqdefault.jpg`}
+          alt={video.title}
         />
-      </div>{' '}
+        {!play ? (
+          <MdPlayCircleFilled className="video-play-icon" />
+        ) : (
+          <MdPauseCircleFilled className="video-play-icon" />
+        )}
+        {play && (
+          <div className="player-wrapper">
+            <ReactPlayer
+              url={video.video}
+              className="react-player"
+              playing={play}
+              config={{
+                youtube: {
+                  playerVars: { showinfo: 0, controls: 1 },
+                },
+              }}
+              width="100%"
+              height="100%"
+            />
+          </div>
+        )}
+      </div>
       <Link to={`/video/${video.slug}`}>{video.title}</Link>
     </div>
   );
