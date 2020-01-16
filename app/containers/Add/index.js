5c53cdd9989fae9a7033a84e85c822ece3eceaab
Bohdan Koldun <koldunbohdan@gmail.com>
2020-01-16 21:53:57 +0200

    wip: add page

--- app/containers/Add/index.js
+++ app/containers/Add/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import { FiHeadphones, FiMic } from 'react-icons/fi';
+import { IoIosMusicalNotes } from 'react-icons/io';
+import { MdRadio } from 'react-icons/md';
+
+import './add.scss';
 
 export default function Add() {
   return (
@@ -7,7 +13,25 @@ export default function Add() {
       <Helmet>
         <title>Добавить песню, альбом, автора</title>
       </Helmet>
-      <div>Добавить:</div>
+      <h1 className="add-header">Добавить:</h1>
+      <div className="add-link">
+        <Link to="/add_song">
+          <FiHeadphones size={48} />
+          <span>Добавить песню</span>
+        </Link>
+        <Link to="/add_album">
+          <IoIosMusicalNotes size={48} />
+          <span>Добавить альбом</span>
+        </Link>
+        <Link to="/add_author">
+          <FiMic size={48} />
+          <span>Добавить музиканта</span>
+        </Link>
+        <Link to="/add_radio">
+          <MdRadio size={48} />
+          <span>Добавить радио</span>
+        </Link>
+      </div>
     </React.Fragment>
   );
 }
