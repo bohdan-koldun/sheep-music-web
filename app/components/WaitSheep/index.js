af45ea673371d1b745d2b8fd4762cc417439df42
Bohdan Koldun <koldunbohdan@gmail.com>
2020-04-18 03:03:21 +0300

    delete gifs

--- app/components/WaitSheep/index.js
+++ app/components/WaitSheep/index.js
@@ -3,13 +3,12 @@ import PropTypes from 'prop-types';
 import './WaitSheep.scss';
 
 function WaitSheep({ message }) {
-  // eslint-disable-next-line global-require
-  const WaitSheepImg = require(`./sheep-wait-images/sheep-wait-${Math.round(
+  const waitSheepImg = `https://sheep.fra1.digitaloceanspaces.com/wait-sheep/sheep-wait-${Math.round(
     0.5 + Math.random() * 11,
-  )}.gif`);
+  )}.gif`;
   return (
     <div className="wait-sheep">
-      <img src={WaitSheepImg} alt="Fun Sheep" />
+      <img src={waitSheepImg} alt="Fun Sheep" />
       {message && <p>{message}</p>}
     </div>
   );
--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -3,7 +3,6 @@ import { FormattedMessage } from 'react-intl';
 import { Helmet } from 'react-helmet';
 
 import messages from './messages';
-import SheepGuiar from './sheep_play_guitar.gif';
 
 export default function NotFound() {
   return (
@@ -18,7 +17,10 @@ export default function NotFound() {
       <h1>
         <FormattedMessage {...messages.header} />
       </h1>
-      <img src={SheepGuiar} alt="shep music" />
+      <img
+        src="https://sheep.fra1.digitaloceanspaces.com/wait-sheep/sheep-wait-10.gif"
+        alt="shep music"
+      />
     </article>
   );
 }
